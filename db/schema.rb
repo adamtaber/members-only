32a54fb3da750519547b835d1899cafe03a22d5d
Adam Taber <altaber@gmail.com>
2022-09-12 15:27:47 -0500

    replace email with username for login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_191829) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_194207) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_191829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
